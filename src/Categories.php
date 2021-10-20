6c5dd63305e7dd5228b0b47a2cd91c369186f940
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-20 12:19:58 +0200

    function to show categories remaded

--- src/Categories.php
+++ src/Categories.php
@@ -20,59 +20,20 @@ class Categories
         return $cat_list;
     }
     /**
-     * Stores categories in a array like-tree shape
-     * @param array $categories_list array containing the categories with their info
+     * Read categories recursively
      */
-    public function saveCats(array $categories_list)
+    public function get_categories(int $root)
     {
-        $parents_data = [];
+        echo '<ul>';
+        echo '<li>'.$root.' '.$this->all_cats[$root]['name'].'</li>';
 
-        $root_depth = Db::getInstance()->getValue('SELECT `level_depth` FROM `ps_category` WHERE `is_root_category` = 1 ');
-        $longest_depth = Db::getInstance()->getValue('SELECT `level_depth` FROM `ps_category` ORDER BY `level_depth` DESC ');
+        $temp = $this->parents_list[$root];
 
-        for ($i = $root_depth; $i <= $longest_depth; $i++) {
-            $parents_data[$i] = "";
+        foreach ($temp as $key => $value) {
+            $this->get_categories($value);
         }
 
-        $cat_arranged = [];
-        foreach ($categories_list as $cat_id => $cat_values) {
+        echo '</ul>';
 
-            $category_id = $cat_id;
-            $node_size = (int) $cat_values['nright'] - (int) $cat_values['nleft']; 
-            $parent = $node_size === 1 ? false : true; //Greater than 1, category is a parent. 1 category isn't a parent
-
-            $category_name = $cat_values['name'];
-            $depth = $cat_values['level_depth'];
-            $insert = $category_id . ' ' . $category_name;
-            //0 depth is index which appends main and the rest of the tree elements.
-            switch ($depth) {
-                case 1:
-                    $cat_arranged[$insert] = [];
-                    $parents_data[1] = $insert;
-                    break;
-                case 2:
-                    $parent ? $cat_arranged[$parents_data[1]][$insert] = [] : $cat_arranged[$parents_data[1]][$insert] = "";
-                    $parents_data[2] = $insert;
-                    break;
-                case 3:
-                    $parent ? $cat_arranged[$parents_data[1]][$parents_data[2]][$insert] = [] : $cat_arranged[$parents_data[1]][$parents_data[2]][$insert] = "";
-                    $parents_data[3] = $insert;
-                    break;
-                case 4:
-                    $parent ? $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$insert] = [] : $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$insert] = "";
-                    $parents_data[4] = $insert;
-                    break;
-                case 5:
-                    $parent ? $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$parents_data[4]][$insert] = [] : $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$parents_data[4]][$insert] = "";
-                    $parents_data[5] = $insert;
-                    break;
-                case 6:
-                    $parent ? $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$parents_data[4]][$parents_data[5]][$insert] = [] : $cat_arranged[$parents_data[1]][$parents_data[2]][$parents_data[3]][$parents_data[4]][$parents_data[5]][$insert] = "";
-                    break;
-                default:
-                    break;
-            }
-        }
-        return $cat_arranged;
     }
 }
