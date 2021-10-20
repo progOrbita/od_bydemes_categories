0f7c5a33438eb8f0c9764f9e57d672640d90b140
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-20 16:08:03 +0200

    properly list done

--- src/Categories.php
+++ src/Categories.php
@@ -26,24 +26,39 @@ class Categories
             $this->all_cats[$value['id_category']] = $value;
         }
         $this->cat_root = Db::getInstance()->getValue('SELECT id_category FROM `ps_category` WHERE is_root_category = 1');
+        $this->root_depth = Db::getInstance()->getValue('SELECT level_depth FROM `ps_category` WHERE is_root_category = 1');
         $this->parents_list = $parents_list;
         return $parents_list;
     }
 
     /**
-     * Read categories recursively
+     * Read categories recursively.
      */
-    public function display_categories(int $root)
+    public function display_categories(int $root, int $current_level, int $previous_level)
     {
-        echo '<ul>';
-        echo '<li>' . $root . ' ' . $this->all_cats[$root]['name'] . '</li>';
+        
+        echo '<li>' . $root . ' ' . $this->all_cats[$root]['name'].'</li>';
+        //Avoid entering in elements who aren't in parents. For each parent this if is reproduced.
+        //If is a child only will print <li> and do current_level--
+        if(isset($this->parents_list[$root])){
+            
+            echo '<ul>';
 
-        $temp = $this->parents_list[$root];
+            $childs = $this->parents_list[$root];
+            //Previous level will takes current level as the actual one of the parent. To "restore" the tree depth
+            $previous_level = $current_level;
 
-        foreach ($temp as $key => $value) {
-            $this->display_categories($value);
+            foreach ($childs as $value) {
+                $this->display_categories($value,$this->all_cats[$value]['level_depth'],$previous_level);
+            }
         }
+        $current_level--;
 
-        echo '</ul>';
+        //Teorically, level go down everytime it exits a child, so if the previous level it's over current(child) level it will ends the <ul>. 
+        //If it's a child nothing happen
+        if($current_level < $previous_level){
+            echo '</ul>';
+        }
+        
     }
 }
