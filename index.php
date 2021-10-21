f3b254ded86e2da2fd4aee252b07a1420a162c80
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-21 13:17:31 +0200

    change colors by icons

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <style>
-            .red{
-                color: red;
-            }
-            .red:hover, .green:hover{
-                cursor: pointer;
-            }
-            .red + ul{
+
+            .disabled + ul{
                 display: none;
             }
-            .green{
-                color: green;
-            }
-            .green + ul{
+            .active + ul{
                 display: block;
+            }
+
+            .active:hover, .disabled:hover{
+                cursor: pointer;
             }
         </style>
     </head>
 <body>
+
 <?php
 
 use OrbitaDigital\OdBydemesCategories\Categories;
@@ -40,17 +38,26 @@ echo $categories->display_categories($root_id);
 ?>
 
 </body>
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script>
         $(document).ready(function(){
-            $(document).on('click','.red',function(){
-                $(this).addClass('green');
-                $(this).removeClass('red');
+
+            $(document).on('click','.disabled',function(){
+                $(this).find("i").remove();
+
+                $(this).append('<i class="bi bi-arrow-down"></i>');
+                $(this).addClass('active');
+                $(this).removeClass('disabled');
             });
-            $(document).on('click','.green',function(){
-                $(this).addClass('red');
-                $(this).removeClass('green');
+
+            $(document).on('click','.active',function(){
+                $(this).find("i").remove();
+                $(this).append('<i class="bi bi-arrow-right"></i>');
+                $(this).addClass('disabled');
+                $(this).removeClass('active');
             });
+
         });
     </script>
 </html>
