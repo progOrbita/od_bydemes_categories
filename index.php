5b403a90b902617e353a90f3d39970061ad43804
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-21 12:08:21 +0200

    added styles and a script to make a dropdown

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <style>
+            .red{
+                color: red;
+            }
+            .red + ul{
+                display: none;
+            }
+            .green{
+                color: green;
+            }
+            .green + ul{
+                display: block;
+            }
+        </style>
+    </head>
+<body>
 <?php
 
 use OrbitaDigital\OdBydemesCategories\Categories;
@@ -15,3 +34,20 @@ $root_id = $categories->get_cat_root_id();
 echo $categories->display_parent(38, $root_id);
 echo $categories->display_categories($root_id);
 
+?>
+
+</body>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(document).on('click','.red',function(){
+                $(this).addClass('green');
+                $(this).removeClass('red');
+            });
+            $(document).on('click','.green',function(){
+                $(this).addClass('red');
+                $(this).removeClass('green');
+            });
+        });
+    </script>
+</html>
