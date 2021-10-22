c9c23976f26c1e3c26e0b9d0e991327bb605c007
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-10-22 11:22:03 +0200

    php display tree and parent functions in body

--- index.php
+++ index.php
@@ -48,24 +48,14 @@ if (isset($_POST['id_cat'])) {
 </head>
 
 <body>
-
     <?php
-
-    use OrbitaDigital\OdBydemesCategories\Categories;
-
-    require_once __DIR__ . '/vendor/autoload.php';
-
-    if (!defined('_PS_VERSION_')) {
-        require_once '../../config/config.inc.php';
-        require_once '../../init.php';
-    }
-
-    $categories = new Categories();
-    $root_id = $categories->get_cat_root_id();
-
-    echo $categories->display_parent(722, $root_id);
-    echo $categories->display_categories($root_id);
-
+    echo
+    '<div id="parents">' .
+        $categories->display_parent(722, $root_id) .
+        '</div>' .
+        '<div id="tree">' .
+        $categories->display_categories($root_id) .
+        '</div>';
     ?>
 
 </body>
